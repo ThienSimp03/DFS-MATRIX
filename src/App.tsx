8d02f9f4922ad58f6b54fc5ebdb0b3699af20113
Thien Simp 03 <104013686+ThienSimp03@users.noreply.github.com>
2023-10-23 16:53:30 +0700

    init pr

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // State
+    const [soDong, setSoDong] = useState<number>(0)
+    const [soCot, setSoCot] = useState<number>(0)
+    const [mangBanDau, setMangBanDau] = useState<number[][]>([])
+    const [mangDuongDaDi, setMangDuongDaDi] = useState<number[][]>([])
+    const [mangKetQua, setMangKetQua] = useState<number[][]>([])
+
+    // Function
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div className='bg-[#1f4287] w-full min-h-screen'>
+            <h1 className='text-4xl font-bold text-center py-5'>Tìm đường đi trong mê cung bằng thuật toán DFS</h1>
+            <form onSubmit={onSubmit} className='flex gap-20 pl-5'>
+                <label>
+                    <p className='text-2xl mb-1'>Nhập số dòng: </p>
+                    <input type='text' className='outline-none p-1' />
+                </label>
+                <button type='submit'>Tạo mảng</button>
+            </form>
+        </div>
+    )
 }
 
-export default App;
+export default App
